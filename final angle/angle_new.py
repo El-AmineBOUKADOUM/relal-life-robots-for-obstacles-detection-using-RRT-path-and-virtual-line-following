a3f45df4aac3c7221ba27f3f8ff5d4bad939147a
mehdimehdi21 <108003110+mehdimehdi21@users.noreply.github.com>
2022-06-25 17:06:13 +0200

    Update angle_new.py

--- final angle/angle_new.py
+++ final angle/angle_new.py
@@ -4,6 +4,7 @@ from numpy import pi
 import pygame
 import sched, time
 import math
+from math import atan2, pi
 
 s = sched.scheduler(time.time, time.sleep)
 t1=0
@@ -18,10 +19,9 @@ pygame.display.set_caption('the man with the if') # Name for the window
 screen.fill(background_colour) #This syntax fills the background colour
 
 
-
 p1 = np.array([300,300])
 p2 = np.array([400,400])
-p3 = np.array([500,500])
+p3 = np.array([450,500])
 
 point1= pygame.draw.circle(screen, (255, 0, 0), p1, 7, 0) #R
 point2= pygame.draw.circle(screen, (0, 255, 0), p2, 7, 0) #G
@@ -29,25 +29,22 @@ Point3= pygame.draw.circle(screen, (0, 0, 255), p3, 7, 0) #B
 
 pygame.display.flip()
 
+
 U = p1 - p2
 V = p3 - p2
 
-cosine_angle = np.dot(U, V) / (np.linalg.norm(U) * np.linalg.norm(V))
-angle = np.arccos(cosine_angle)
-
-
-
-# adjust for hemisphere
-if V[0] < 0: angle=360 - np.degrees(angle)
-if V[0] > 0: angle=np.degrees(angle)
-
+cosine_angle = np.dot(U,V) / (np.linalg.norm(U) * np.linalg.norm(V))
+angle = np.arctan2(abs(np.cross(V, U)),np.dot(U, V))*360/(2*pi)
+if (np.cross(V, U) > 0): angle = 360 - angle
 
 
 while True:
-    print (angle)
     
+    print(angle)
+   
+
     if angle == 180:
-        
+            
         print ('move forward')
         time.sleep(2)
     elif angle < 180:
@@ -56,9 +53,8 @@ while True:
     elif angle > 180:
         print ('translate right')
         time.sleep(2)
-    
-    
-
+        
+       
 running = True
 while running:
     for event in pygame.event.get():
